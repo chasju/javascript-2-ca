e6d3fa51b4b1d1b47b33b096a0f63122c282e316
chasju <91468816+chasju@users.noreply.github.com>
2022-10-09 18:16:19 +0200

    set placeholder profile picture

--- src/js/handlers/setProfilePicture.mjs
+++ src/js/handlers/setProfilePicture.mjs
@@ -4,6 +4,11 @@ export async function setProfilePicture() {
   try {
     const profile = await getProfile();
     const pictureContainer = document.querySelector(".profile-picture");
+
+    if (!profile.avatar) {
+      profile.avatar = "https://image.cnbcfm.com/api/v1/image/105897632-1557241558937avatar-e1541360922907.jpg?v=1664130328&w=1920&h=1080";
+    }
+
     if (location.pathname === "/pages/profile/") {
       pictureContainer.innerHTML = `<img src="${profile.avatar}" alt="" class="img-fluid border border-secondary border-5 rounded-circle card-profile-pic-profile" style="--bs-border-opacity: 0.4">`;
     } else {
