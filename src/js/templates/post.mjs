a904f9ed4b7a1ecc55197625ceb9b04873e7f8e7
chasju <91468816+chasju@users.noreply.github.com>
2022-10-10 14:52:27 +0200

    Destructuring and JSDocs

--- src/js/templates/post.mjs
+++ src/js/templates/post.mjs
@@ -1,6 +1,12 @@
-import { removePost } from "../api/posts/delete.mjs";
-import { getHomeFeedPosts } from "../handlers/getPosts.mjs";
-import { getProfileFeedPosts } from "../handlers/getPosts.mjs";
+import { deletePost } from "../handlers/deletePost.mjs";
+
+/**
+ * Function will insert values from postData and is a template for how a post should look like.
+ *
+ * @param {object} postData
+ *
+ * @returns The function returns the post template and is being used in the renderPostTemplate function.
+ */
 
 export function postTemplate(postData) {
   const post = document.createElement("div");
@@ -11,45 +17,43 @@ export function postTemplate(postData) {
       break;
     }
 
-    // API variables
-    const handle = postData[i].author.name;
-    let avatar = postData[i].author.avatar;
-    const date = postData[i].updated;
-    const postBody = postData[i].body;
-    const postId = postData[i].id;
+    // Destructuring API into variables
+
+    const { author, updated, body, id } = postData[i];
 
     // Setting placeholder for profiles without avatar
 
-    if (!avatar) {
-      avatar = "https://image.cnbcfm.com/api/v1/image/105897632-1557241558937avatar-e1541360922907.jpg?v=1664130328&w=1920&h=1080";
+    if (!author.avatar) {
+      author.avatar = "https://image.cnbcfm.com/api/v1/image/105897632-1557241558937avatar-e1541360922907.jpg?v=1664130328&w=1920&h=1080";
     }
 
     // Converting date format
 
-    const postUpdated = new Date(date);
+    const postUpdated = new Date(updated);
     const format = { year: "numeric", month: "long", day: "numeric" };
-    const updated = postUpdated.toLocaleDateString("en-US", format);
+    const updatedDate = postUpdated.toLocaleDateString("en-US", format);
 
     post.innerHTML += `<div class="row bg-primary rounded-1 pt-3 pb-3 mb-4" style="--bs-bg-opacity: 0.1">
               <div class="col-1 border-0 w-25 bg-unset">
                 <div class="d-flex justify-content-between align-items-center">
                   <div class="d-flex feed-profile-pic" style="width:50px;height:50px;">
                     <div class="mr-2">
-                      <a href="/pages/profile/"><img class="rounded-circle" width="50" src="${avatar}" alt="thumbnail" style="width:100%;height:100%;object-fit:cover;"/></a>
+                      <a href="#"><img class="rounded-circle" width="50" src="${author.avatar}" alt="thumbnail" style="width:100%;height:100%;object-fit:cover;"/></a>
                     </div>
                   </div>
                 </div>
               </div>
               <div class="card-body card-body-feed mb-3 col-7 p-0">
                 <div class="ml-2 mb-1 d-flex gap-3">
-                  <div class="h7 text-primary fs-6">@${handle}</div>
+                  <div class="h7 text-primary fs-6"><a href="/pages/post/?id=${id}">@${author.name}</a></div>
                   <p class="text-primary">|</p>
-                  <div class="h7 text-primary">${updated}</div>
+                  <div class="h7 text-primary">${updatedDate}</div>
                 </div>
-                <p class="card-text">${postBody}</p>
+                <p class="card-text">${body}</p>
                 <div class="d-flex gap-3">
-                  <a href="/pages/edit/?id=${postId}" class="h7 text-primary edit-post">Edit</a>
-                  <div id="${postId}" class="h7s text-primary delete-post">Delete Post</div>
+                  <a href="/pages/edit/?id=${id}" class="h7 text-primary edit-post">Edit</a>
+                  <a href="/pages/post/?id=${id}" class="h7 text-primary edit-post">Comment</a>
+                  <div id="${id}" class="h7s text-primary delete-post" role="button">Delete Post</div>
                 </div>
               </div>
             </div>`;
@@ -58,20 +62,18 @@ export function postTemplate(postData) {
   return post;
 }
 
+/**
+ * Function will render a post based on its parameters.
+ *
+ * @param {object, array} postData This is post information that will be inserted into its parent container
+ * @param {Element} parent This is the parent container
+ *
+ * @returns renders the post template
+ */
+
 export function renderPostTemplate(postData, parent) {
   parent.append(postTemplate(postData));
 
   // Set delete function after API result is loaded
-  const deleteButton = document.querySelectorAll(".delete-post");
-
-  deleteButton.forEach((button) => {
-    button.addEventListener("click", (e) => {
-      removePost(e.target.id);
-      if (location.pathname === "/pages/profile/") {
-        getProfileFeedPosts();
-      } else {
-        getHomeFeedPosts();
-      }
-    });
-  });
+  deletePost(postData);
 }
